f47f7bd6076d5dd3281c10f1727c1e3ad5414614
vedavignesh <Veda@2021>
2022-11-12 14:20:33 +0530

    terra

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,7 +2,7 @@
 resource "aws_vpc" "vpc" {
   cidr_block              = var.vpc_cidr 
   instance_tenancy        = "default"
-  enable_dns_hostnames    = true
+  enable_dns_hostnames    = tru
 
   tags      = {
     Name    = "${var.project_name}-vpc"
