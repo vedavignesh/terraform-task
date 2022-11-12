c61d962b096ff0ed4660a25bea47d650b011232b
vedavignesh <Veda@2021>
2022-11-12 14:22:52 +0530

    terraform-code-2

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,7 +2,7 @@
 resource "aws_vpc" "vpc" {
   cidr_block              = var.vpc_cidr 
   instance_tenancy        = "default"
-  enable_dns_hostnames    = tru
+  enable_dns_hostnames    = true
 
   tags      = {
     Name    = "${var.project_name}-vpc"
